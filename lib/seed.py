29dbb7083700a2784c7b38d95a1bb9ab99251cb5
TracyAntonia <aantonjuguna@gmail.com>
2023-09-11 11:01:12 +0300

    completed assignments

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,23 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    session.query(Game).delete
+    session.commit()
+
+    print("Seeding games...")
+
+    games = [
+        Game(
+            title=fake.unique.name(), 
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)  
+        )
+        for _ in range(50) 
+    ]
+
+    session.bulk_save_objects(games)
+    session.commit()
+
+    print("Seeding completed.")
